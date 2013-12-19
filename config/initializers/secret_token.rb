76262cc6f4d224011dd1938e60a4ad88fd28c653
Quickfront <diego@quickfront.com.br>
2013-12-19 19:22:09 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quicklink::Application.config.secret_key_base = 'c0c55be73c5542a5249d7a478c19b1f0a9e9d82402fc80ed8830d5481ed3555fae1f800cd5c758da882e1b79c759325569237e2c05d03667bf772c7fb7ba9de3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quicklink::Application.config.session_store :cookie_store, key: '_quicklink_session'
